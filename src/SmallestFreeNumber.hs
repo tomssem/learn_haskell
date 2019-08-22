8fbef1dd5234f1209cc50a9cef7c6acf2d2179cf
Tom N <tfwnicholson+1@gmail.com>
2019-08-22 18:28:33 +0900

    Implement checklist

--- src/SmallestFreeNumber.hs
+++ src/SmallestFreeNumber.hs
@@ -4,10 +4,18 @@
 
 module SmallestFreeNumber where
 
+import Data.Array
+
 type Nat = Int
+
+testList = [08, 23, 09, 00, 12, 11 , 01, 10, 13, 07, 41, 04, 14, 21, 05, 17, 03, 19, 03, 06] :: [Nat]
 
 minfree1 :: [Nat] -> Nat
 minfree1 xs = head ([0..] \\ xs)
 
 (\\) :: Eq a => [a] -> [a] -> [a]
 us \\ vs = filter (`notElem` vs) us
+
+checklist :: [Nat] -> Array Nat Bool
+checklist xs = accumArray (||) False (0, n) (zip (filter (<= n) xs) (repeat True))
+              where n = length xs
--- tests/SmallestFreeNumberSpec.hs
+++ tests/SmallestFreeNumberSpec.hs
@@ -2,13 +2,25 @@ module SmallestFreeNumberSpec (spec) where
 
 import Test.Hspec
 import SmallestFreeNumber
+import Data.Array
 
 spec :: Spec
 spec =
-  describe "minfree1" $ do
-    it "returns 0 when given empty list" $
-      minfree1 [] `shouldBe` 0
-    it "returns 1 when given a list containined only 1" $
-      minfree1 [0] `shouldBe` 1
-    it "returns 0 when given a list containing any list of numbers greater than 1" $
-      minfree1 [1] `shouldBe` 0
+  describe "SmallestFreeNumber" $ do
+    describe "minfree1" $ do
+      it "returns 0 when given empty list" $
+        minfree1 [] `shouldBe` 0
+      it "returns 1 when given a list containined only 1" $
+        minfree1 [0] `shouldBe` 1
+      it "returns 0 when given a list containing any list of numbers greater than 1" $
+        minfree1 [1] `shouldBe` 0
+
+    describe "countlist" $ do
+      it "Returns array length 1 consisting of False when given empty array" $
+        checklist [] `shouldBe` listArray (0, 0) [False]
+      it "returns array of [True, False] when xs consists of a single 0" $
+        checklist [0] `shouldBe` listArray (0, 1) [True, False]
+      it "returns array of [False, True] when xs consists of a single 0" $
+        checklist [1] `shouldBe` listArray (0, 1) [False, True]
+      it "returns correct array for longer input" $
+        checklist [1, 3, 5] `shouldBe` listArray (0, 3) [False, True, False, True, False]
