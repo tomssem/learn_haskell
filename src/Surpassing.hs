ea072492b6b01b0147b3d99510fea9aa2c532b8a
Tom N <tfwnicholson+1@gmail.com>
2019-09-15 19:19:50 +0900

    Complete specification of surpassing problem

--- src/Surpassing.hs
+++ src/Surpassing.hs
@@ -2,7 +2,10 @@ module Surpassing where
 
 msc :: (Ord a) => [a] -> Maybe Int
 msc [] = Nothing
-msc xs = Just $ length xs
+msc xs = Just $ maximum [scount z zs | z : zs <- myTails xs]
+
+scount :: (Ord a) => a -> [a] -> Int
+scount x xs = length $ filter (x <) xs
 
 myTails :: [a] -> [[a]]
 myTails [] = []
--- tests/SurpassingSpec.hs
+++ tests/SurpassingSpec.hs
@@ -13,10 +13,19 @@ spec =
         msc ([] :: [Int]) `shouldBe` Nothing
       it "returns 0 when single element list is given" $
         msc [0] `shouldBe` Just 0
-      it "returns 0 when array in ascending order is given" $
-        msc [1, 2, 3, 4] `shouldBe` Just 0
-      it "returns length of list minus one when given contiguous descending list" $
-        msc [6, 5, 4, 3, 2] `shouldBe` Just 4
+      it "returns 0 when array in descending order is given" $
+        msc [1, 2, 3, 4] `shouldBe` Just 3
+      it "returns length of list minus one when given contiguous ascending list" $
+        msc [6, 5, 4, 3, 2] `shouldBe` Just 0
+    describe "scount" $ do
+      it "returns 0 for an empty list" $
+        scount 0 [] `shouldBe` 0
+      it "returns 0 when value is bigger than all elements in list" $
+        scount 4 [0, 2, 3, 1] `shouldBe` 0
+      it "returns 0 when list counts elements all the same as value" $
+        scount 10 (replicate 100 10) `shouldBe` 0
+      it "returns length of list when all values are greater" $
+        scount 0 [1..100] == 100
     describe "myTails" $
       it "is same as tails with empty list filtered out" $ property $
         \x -> (myTails x) == (filter (not . null) $ tails (x :: [Int]))
