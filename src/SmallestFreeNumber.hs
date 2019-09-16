b7b4ff40353aa87e5539a593d7621b0625fd6425
Tom N <tfwnicholson+1@gmail.com>
2019-09-16 10:42:55 +0900

    Only unique elements for smallest free number

--- src/SmallestFreeNumber.hs
+++ src/SmallestFreeNumber.hs
@@ -13,13 +13,23 @@ type Nat = Int
 testList :: [Nat]
 testList = [08, 23, 09, 00, 12, 11 , 01, 10, 13, 07, 41, 04, 14, 21, 05, 17, 03, 19, 03, 06] :: [Nat]
 
-checkIfAllPos :: (Integral a) => [a] -> ([a] -> b) -> Maybe b
-checkIfAllPos xs f = if not $ all (>= 0) xs
+allPos :: (Integral a) => [a] -> Bool
+allPos = all (>= 0)
+
+noDuplicates :: (Integral a) => [a] -> Bool
+noDuplicates [] = True
+noDuplicates (x:xs) = (x `notElem` xs ) || noDuplicates xs
+
+checkInvariants :: (Integral a) => [a] -> Bool
+checkInvariants xs = all ($ xs) [noDuplicates, allPos]
+
+guardInvariants :: (Integral a) => [a] -> ([a] -> b) -> Maybe b
+guardInvariants xs f = if not $ checkInvariants xs
                      then Nothing
                      else Just $ f xs
 
 minfree1 :: [Nat] -> Maybe Nat
-minfree1 xs = checkIfAllPos xs (\x -> head ([0..] \\ x))
+minfree1 xs = guardInvariants xs (\x -> head ([0..] \\ x))
 
 checklist :: [Nat] -> Array Int Bool
 checklist xs = accumArray (||) False (0, n) (zip (filter (<= n) (map fromIntegral xs)) (repeat True))
@@ -30,11 +40,11 @@ countlist xs = accumArray (+) 0 (0, n) (zip (map fromIntegral xs) (repeat 1))
               where n = foldl max 0 xs -- 0 is identity element for max on natural numbers
 
 minfreeArray :: [Nat] -> Maybe Nat
-minfreeArray xs = checkIfAllPos xs (\x -> length $ takeWhile id (elems $ checklist x))
+minfreeArray xs = guardInvariants xs (\x -> length $ takeWhile id (elems $ checklist x))
 
 countsort :: [Nat] -> Maybe [Nat]
 countsort [] = Just []
-countsort xs = checkIfAllPos xs (\z -> concat [replicate (fromIntegral k) x | (x, k) <- assocs (countlist z)])
+countsort xs = guardInvariants xs (\z -> concat [replicate (fromIntegral k) x | (x, k) <- assocs (countlist z)])
 
 checklistProsaic xs = runSTArray $ do
                                     a <- newArray (0, n) False
@@ -43,7 +53,7 @@ checklistProsaic xs = runSTArray $ do
                             where n = length xs
 
 minfreeDivConq :: [Nat] -> Maybe Nat
-minfreeDivConq xs = checkIfAllPos xs (\x -> minfrom 0 (length x, x))
+minfreeDivConq xs = guardInvariants xs (\x -> minfrom 0 (length x, x))
 
 minfrom :: Nat -> (Int, [Nat]) -> Nat
 minfrom a (n, xs) | null xs            = a
