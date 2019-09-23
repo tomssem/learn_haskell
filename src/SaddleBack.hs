4bbae73743f3918a135de2a7a8599324f5750ed1
Tom N <tfwnicholson+1@gmail.com>
2019-09-23 18:14:19 +0900

    Add find method for saddle search

--- src/SaddleBack.hs
+++ src/SaddleBack.hs
@@ -2,3 +2,12 @@ module SaddleBack where
 
 invert :: (Int -> Int -> Int) -> Int -> [(Int, Int)]
 invert f z = [(x, y) | x <- [0..z], y <- [0..z], f x y == z]
+
+invertFaster :: (Int -> Int -> Int) -> Int -> [(Int, Int)]
+invertFaster f z = [(x, y) | x <- [0..z - f 0 0], y <- [0..z - x - f 0 0], f x y == z]
+
+find :: (Int, Int) -> (Int -> Int -> Int) -> Int -> [(Int, Int)]
+find (u, v) f z = [(x, y) | x <- [u..z], y <- [v, v - 1 .. 0], f x y == z]
+
+invertFancy :: (Int -> Int -> Int) -> Int -> [(Int, Int)]
+invertFancy f z = find (0, z) f z
--- tests/SaddleBackSpec.hs
+++ tests/SaddleBackSpec.hs
@@ -4,11 +4,24 @@ import SaddleBack
 import Test.Hspec
 import Test.QuickCheck
 
+almostMultiplication :: Int -> Int -> Int
+almostMultiplication x y = (x + 1) * (y + 1)
+
 spec :: Spec
 spec =
-  describe "SaddleBack" $
-    describe "saddleback" $ do
+  describe "SaddleBack" $ do
+    describe "invert" $ do
       it "works for addition" $
         invert (+) 10 `shouldBe` [(0,10),(1,9),(2,8),(3,7),(4,6),(5,5),(6,4),(7,3),(8,2),(9,1),(10,0)]
-      it "works for multiplication" $
-        invert (*) 10 `shouldBe` [(1,10),(2,5),(5,2),(10,1)]
+      it "works for 'multiplication'" $
+        invert almostMultiplication 10 `shouldBe` [(0,9),(1,4),(4,1),(9,0)]
+    describe "invertFaster" $ do
+      it "should be the same as invert for addition" $ property $
+        \x -> invert (+) x == invertFaster (+) x
+      it "should be the same as invert for 'multiplication'" $ property $
+        \x -> invert almostMultiplication x == invertFaster almostMultiplication x
+    describe "invertFancy" $ do
+      it "should be the same as invert for addition" $ property $
+        \x -> invert (+) x == invertFancy (+) x
+      it " should be the as invert for 'multiplication" $ property $
+        \x -> invert almostMultiplication x == invertFancy almostMultiplication x
